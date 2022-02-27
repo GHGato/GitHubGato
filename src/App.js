5ca7ee99a2dceb2f68bd00ecab54556afc610e91
Kev <haokliao@gmail.com>
2022-02-27 04:09:16 -0500

    fixing router issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-
-import {
-  BrowserRouter,
+// import {HashRouter as Router} from 'react-router-dom'
+import {  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
@@ -11,6 +10,13 @@ import Branch from './pages/Branch.js'
 
 const App =() => {
   return (
+    // <Router>
+    //   {/* <Routes>
+    //     <Route path="/GitHubGato" element={<Landing />} />
+    //     <Route path="/GitHubGato/clone" element={<Clone />} />
+    //     <Route path="/GitHubGato/branch" element={<Branch />} />
+    //   </Routes> */}
+    // </Router>
     <BrowserRouter>
     <Routes>
       <Route path="/GitHubGato" element={<Landing />} />
